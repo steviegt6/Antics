40d09c2deafdc1aff83746d588cd295d08965739
Stevie <xxlennygamerxx@gmail.com>
2021-06-15 02:11:00 -0700

    wip loading too tired to continue tonight

--- Antics.SomeLoader/Program.cs
+++ Antics.SomeLoader/Program.cs
@@ -2,118 +2,146 @@
 using System.IO;
 using Antics.SomeLoader.CLI;
 using Antics.SomeLoader.CLI.Personal;
+using SomeAntics.API;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 
 namespace Antics.SomeLoader
 {
-    /// <summary>
-    ///     Entry-point.
-    /// </summary>
-    public static class Launcher
-    {
-        /// <summary>
-        ///     Raw version.
-        /// </summary>
-        public const string VersionString = "0.1.0.0";
+	/// <summary>
+	///     Entry-point.
+	/// </summary>
+	public static class Launcher
+	{
+		/// <summary>
+		///     Raw version.
+		/// </summary>
+		public const string VersionString = "0.1.0.0";
 
-        /// <summary>
-        ///     Version parsed to a <see cref="Version"/> instance.
-        /// </summary>
-        public static readonly Version Version = Version.Parse(VersionString);
+		/// <summary>
+		///     Version parsed to a <see cref="Version"/> instance.
+		/// </summary>
+		public static readonly Version Version = Version.Parse(VersionString);
 
-        /// <summary>
-        ///     Universal instance of <see cref="SomeAntics.SomeAntics"/>.
-        /// </summary>
-        public static SomeAntics.SomeAntics AnticsInstance { get; private set; }
+		/// <summary>
+		///     Universal instance of <see cref="SomeAntics.SomeAntics"/>.
+		/// </summary>
+		public static SomeAntics.SomeAntics AnticsInstance { get; private set; }
 
-        /// <summary>
-        /// </summary>
-        public static void Main()
-        {
-            Console.Title = "Antics.SomeLoader - by convicted tomatophile";
+		public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            PersonalCLI.WriteStaticText();
-            CommandLineUtilities.WriteLineWithColor("Launching SomeAntics...", ConsoleColor.DarkGray);
+		/// <summary>
+		/// </summary>
+		public static void Main() {
+			Console.Title = "Antics.SomeLoader - by convicted tomatophile";
 
-            /*if (!File.Exists("SomeAntics.exe") && OperatingSystem.IsWindows())
-            {
-                CommandLineUtilities.WriteLineWithColor(
-                    "SomeAntics.exe could not be found. You are using a Windows OS, please supply the loader with a working executable!" +
-                    "\nPress <enter> to exit.", ConsoleColor.Red);
-                Console.ReadLine();
-                return;
-            }*/
+			PersonalCLI.WriteStaticText();
+			CommandLineUtilities.WriteLineWithColor("Launching SomeAntics...", ConsoleColor.DarkGray);
 
-            if (!File.Exists("SomeAntics.dll"))
-            {
-                CommandLineUtilities.WriteLineWithColor(
-                    "SomeAntics.dll could not be found, please supply the loader with a working DLL." +
-                    "\nPress <enter> to exit.", ConsoleColor.Red);
-                Console.ReadLine();
-                return;
-            }
+			/*if (!File.Exists("SomeAntics.exe") && OperatingSystem.IsWindows())
+			{
+				CommandLineUtilities.WriteLineWithColor(
+					"SomeAntics.exe could not be found. You are using a Windows OS, please supply the loader with a working executable!" +
+					"\nPress <enter> to exit.", ConsoleColor.Red);
+				Console.ReadLine();
+				return;
+			}*/
 
-            /*string processName;
-            string arguments = "";
+			if (!File.Exists("SomeAntics.dll")) {
+				CommandLineUtilities.WriteLineWithColor(
+					"SomeAntics.dll could not be found, please supply the loader with a working DLL." +
+					"\nPress <enter> to exit.", ConsoleColor.Red);
+				Console.ReadLine();
+				return;
+			}
 
-            if (OperatingSystem.IsWindows())
-                processName = "SomeAntics.exe";
-            else
-            {
-                processName = "bash"; // If bash doesn't work then I don't know what you tell 'ya :^)
-                arguments = "dotnet SomeAntics.dll"; // *should* launch the DLL
-            }
+			/*string processName;
+			string arguments = "";
 
-            ProcessStartInfo startInfo = new(processName)
-            {
-                RedirectStandardError = true,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-                WindowStyle = ProcessWindowStyle.Maximized,
-                CreateNoWindow = false,
-                Arguments = arguments
-            };
+			if (OperatingSystem.IsWindows())
+				processName = "SomeAntics.exe";
+			else
+			{
+				processName = "bash"; // If bash doesn't work then I don't know what you tell 'ya :^)
+				arguments = "dotnet SomeAntics.dll"; // *should* launch the DLL
+			}
 
-            Process process = Process.Start(startInfo);
+			ProcessStartInfo startInfo = new(processName)
+			{
+				RedirectStandardError = true,
+				RedirectStandardInput = true,
+				RedirectStandardOutput = true,
+				WindowStyle = ProcessWindowStyle.Maximized,
+				CreateNoWindow = false,
+				Arguments = arguments
+			};
 
-            if (process is null)
-            {
-                CommandLineUtilities.WriteLineWithColor("Process failed to start. Press <enter> to exit.",
-                    ConsoleColor.Red);
-                Console.ReadLine();
-                return;
-            }
+			Process process = Process.Start(startInfo);
 
-            while (!process.HasExited)
-            {
+			if (process is null)
+			{
+				CommandLineUtilities.WriteLineWithColor("Process failed to start. Press <enter> to exit.",
+					ConsoleColor.Red);
+				Console.ReadLine();
+				return;
+			}
 
-            }*/
+			while (!process.HasExited)
+			{
 
-            try
-            {
-                /*Type programType = typeof(SomeAntics.Program);
-                MethodInfo mainMethod = programType.GetMethod("Main", BindingFlags.Static | BindingFlags.NonPublic);
+			}*/
 
-                if (mainMethod is null)
-                {
-                    CommandLineUtilities.WriteLineWithColor("Main method was null! Press <enter> to exit.", ConsoleColor.Red);
-                    Console.ReadLine();
-                    return;
-                }
+			Directory.CreateDirectory(Path.Combine(ExecutableDirectory, "Mods"));
+			CommandLineUtilities.WriteLineWithColor("Loading mods in /Mods/...", ConsoleColor.DarkGray);
+			List<Assembly> assemblies =
+				(from modDirectory in Directory.GetDirectories(Path.Combine(ExecutableDirectory, "Mods"), "*",
+						SearchOption.AllDirectories)
+					select new DirectoryInfo(modDirectory)
+					into dirInfo
+					from file in dirInfo.GetFiles()
+					where file.Name.Equals($"{dirInfo.Name}.dll")
+					select Assembly.LoadFile(file.ToString())).ToList();
 
-                mainMethod.Invoke(null, null);*/
+			foreach (Assembly assembly in assemblies) {
+				AppDomain.CurrentDomain.Load(assembly.GetName());
 
-                using SomeAntics.SomeAntics antics = new();
-                AnticsInstance = antics;
-                AnticsInstance.Run();
-            }
-            catch (Exception e)
-            {
-                CommandLineUtilities.WriteLineWithColor("Caught error, closing safely:", ConsoleColor.Yellow);
-                CommandLineUtilities.WriteLineWithColor(e.ToString(), ConsoleColor.Red);
-                CommandLineUtilities.WriteLineWithColor("Press <enter> to exit.", ConsoleColor.DarkGray);
-            }
+				foreach (Type type in assembly.GetTypes())
+					if (type == typeof(Mod) && !type.IsAbstract && type.GetConstructor(Array.Empty<Type>()) != null)
+						ModManager.ModAssemblies.Add(assembly); // add if this is a mod assembly
+			}
 
-            // Exit once the process has exited, maybe don't, we'll determine later
-        }
-    }
+			foreach (Assembly modAssembly in ModManager.ModAssemblies) {
+				Mod mod = (Mod) Activator.CreateInstance(modAssembly.GetTypes().First(x => x == typeof(Mod) && !x.IsAbstract && x.GetConstructor(Array.Empty<Type>()) != null));
+				Debug.Assert(mod != null, nameof(mod) + " != null");
+				mod.Code = modAssembly;
+			}
+
+			try {
+				/*Type programType = typeof(SomeAntics.Program);
+				MethodInfo mainMethod = programType.GetMethod("Main", BindingFlags.Static | BindingFlags.NonPublic);
+
+				if (mainMethod is null)
+				{
+					CommandLineUtilities.WriteLineWithColor("Main method was null! Press <enter> to exit.", ConsoleColor.Red);
+					Console.ReadLine();
+					return;
+				}
+
+				mainMethod.Invoke(null, null);*/
+
+				using SomeAntics.SomeAntics antics = new();
+				AnticsInstance = antics;
+				AnticsInstance.Run();
+			}
+			catch (Exception e) {
+				CommandLineUtilities.WriteLineWithColor("Caught error, closing safely:", ConsoleColor.Yellow);
+				CommandLineUtilities.WriteLineWithColor(e.ToString(), ConsoleColor.Red);
+				CommandLineUtilities.WriteLineWithColor("Press <enter> to exit.", ConsoleColor.DarkGray);
+			}
+
+			// Exit once the process has exited, maybe don't, we'll determine later
+		}
+	}
 }
--- patches/Antics.SomeLoader/SomeAntics/API/ModManager.cs
+++ patches/Antics.SomeLoader/SomeAntics/API/ModManager.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Reflection;
 
 namespace SomeAntics.API
 {
-	public class ModManager
+	public static class ModManager
 	{
 		public static ReadOnlyCollection<Mod> ModsCollection => Mods.AsReadOnly();
 
 		internal static List<Mod> Mods { get; } = new();
+
+		internal static List<Assembly> ModAssemblies { get; } = new();
 	}
 }
