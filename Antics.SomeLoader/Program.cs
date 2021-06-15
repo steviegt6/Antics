16ddeec0ea3a234e555734da0829d045bee76d14
Stevie <xxlennygamerxx@gmail.com>
2021-06-15 14:21:45 -0700

    some project unification, debugging issues

--- Antics.SomeLoader/Program.cs
+++ Antics.SomeLoader/Program.cs
@@ -2,10 +2,6 @@
 using System.IO;
 using Antics.SomeLoader.CLI;
 using Antics.SomeLoader.CLI.Personal;
-using SomeAntics.API;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
 
 namespace Antics.SomeLoader
@@ -32,116 +28,91 @@ namespace Antics.SomeLoader
 
 		public static string ExecutableDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
+		public static string LibraryDirectory => ExecutableDirectory; // change eventually
+
 		/// <summary>
 		/// </summary>
 		public static void Main() {
 			Console.Title = "Antics.SomeLoader - by convicted tomatophile";
 
 			PersonalCLI.WriteStaticText();
+
+			CommandLineUtilities.WriteLineWithColor("Hooking into AssemblyResolve", ConsoleColor.DarkGray);
+
+			AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+
+			CommandLineUtilities.WriteLineWithColor("Asserting the presence of a modded copy of SomeAntics...",
+				ConsoleColor.DarkGray);
+
+			AssertModdedPresence();
+
 			CommandLineUtilities.WriteLineWithColor("Launching SomeAntics...", ConsoleColor.DarkGray);
 
-			/*if (!File.Exists("SomeAntics.exe") && OperatingSystem.IsWindows())
-			{
-				CommandLineUtilities.WriteLineWithColor(
-					"SomeAntics.exe could not be found. You are using a Windows OS, please supply the loader with a working executable!" +
-					"\nPress <enter> to exit.", ConsoleColor.Red);
-				Console.ReadLine();
-				return;
-			}*/
-
-			if (!File.Exists("SomeAntics.dll")) {
-				CommandLineUtilities.WriteLineWithColor(
-					"SomeAntics.dll could not be found, please supply the loader with a working DLL." +
-					"\nPress <enter> to exit.", ConsoleColor.Red);
-				Console.ReadLine();
-				return;
-			}
-
-			/*string processName;
-			string arguments = "";
-
-			if (OperatingSystem.IsWindows())
-				processName = "SomeAntics.exe";
-			else
-			{
-				processName = "bash"; // If bash doesn't work then I don't know what you tell 'ya :^)
-				arguments = "dotnet SomeAntics.dll"; // *should* launch the DLL
-			}
-
-			ProcessStartInfo startInfo = new(processName)
-			{
-				RedirectStandardError = true,
-				RedirectStandardInput = true,
-				RedirectStandardOutput = true,
-				WindowStyle = ProcessWindowStyle.Maximized,
-				CreateNoWindow = false,
-				Arguments = arguments
-			};
-
-			Process process = Process.Start(startInfo);
-
-			if (process is null)
-			{
-				CommandLineUtilities.WriteLineWithColor("Process failed to start. Press <enter> to exit.",
-					ConsoleColor.Red);
-				Console.ReadLine();
-				return;
-			}
-
-			while (!process.HasExited)
-			{
-
-			}*/
-
-			Directory.CreateDirectory(Path.Combine(ExecutableDirectory, "Mods"));
-			CommandLineUtilities.WriteLineWithColor("Loading mods in /Mods/...", ConsoleColor.DarkGray);
-			List<Assembly> assemblies =
-				(from modDirectory in Directory.GetDirectories(Path.Combine(ExecutableDirectory, "Mods"), "*",
-						SearchOption.AllDirectories)
-					select new DirectoryInfo(modDirectory)
-					into dirInfo
-					from file in dirInfo.GetFiles()
-					where file.Name.Equals($"{dirInfo.Name}.dll")
-					select Assembly.LoadFile(file.ToString())).ToList();
-
-			foreach (Assembly assembly in assemblies) {
-				AppDomain.CurrentDomain.Load(assembly.GetName());
-
-				foreach (Type type in assembly.GetTypes())
-					if (type == typeof(Mod) && !type.IsAbstract && type.GetConstructor(Array.Empty<Type>()) != null)
-						ModManager.ModAssemblies.Add(assembly); // add if this is a mod assembly
-			}
-
-			foreach (Assembly modAssembly in ModManager.ModAssemblies) {
-				Mod mod = (Mod) Activator.CreateInstance(modAssembly.GetTypes().First(x => x == typeof(Mod) && !x.IsAbstract && x.GetConstructor(Array.Empty<Type>()) != null));
-				Debug.Assert(mod != null, nameof(mod) + " != null");
-				mod.Code = modAssembly;
-			}
-
 			try {
-				/*Type programType = typeof(SomeAntics.Program);
-				MethodInfo mainMethod = programType.GetMethod("Main", BindingFlags.Static | BindingFlags.NonPublic);
-
-				if (mainMethod is null)
-				{
-					CommandLineUtilities.WriteLineWithColor("Main method was null! Press <enter> to exit.", ConsoleColor.Red);
-					Console.ReadLine();
-					return;
-				}
-
-				mainMethod.Invoke(null, null);*/
-
 				using SomeAntics.SomeAntics antics = new();
 				AnticsInstance = antics;
 				AnticsInstance.Run();
+				// Run(); // Separate method to prevent an immediate load attempt
 			}
 			catch (Exception e) {
 				CommandLineUtilities.WriteLineWithColor("Caught error, closing safely:", ConsoleColor.Yellow);
 				CommandLineUtilities.WriteLineWithColor(e.ToString(), ConsoleColor.Red);
 				CommandLineUtilities.WriteLineWithColor("Press <enter> to exit.", ConsoleColor.DarkGray);
-			}
+			} // Exit once the process has exited, maybe don't, we'll determine later
+		}
 
-			// Exit once the process has exited, maybe don't, we'll determine later
+		private static void Run() {
+			using SomeAntics.SomeAntics antics = new();
+			AnticsInstance = antics;
+			AnticsInstance.Run();
+		}
+
+#nullable enable
+		private static Assembly? CurrentDomainOnAssemblyResolve(object? sender, ResolveEventArgs args) {
+			try {
+				AssemblyName name = new(args.Name);
+
+				foreach (FileInfo file in new DirectoryInfo(LibraryDirectory).EnumerateFiles("*.dll"))
+					if (name.Name != null && name.Name.Equals(AssemblyName.GetAssemblyName(file.FullName).Name,
+						StringComparison.OrdinalIgnoreCase)) {
+						CommandLineUtilities.WriteLineWithColor($"Loaded assembly: {name.Name}", ConsoleColor.DarkGray);
+						return Assembly.LoadFrom(file.FullName);
+					}
+
+				CommandLineUtilities.WriteLineWithColor($"Failed to load assembly: {name.Name}", ConsoleColor.Yellow);
+				return null;
+			}
+			catch (Exception e) {
+				CommandLineUtilities.WriteLineWithColor(
+					$"Error while attempting to resolve assembly \"{args.Name}\": {e}", ConsoleColor.DarkRed);
+				return null;
+			}
+		}
+#nullable restore
+
+		private static void AssertModdedPresence() {
+			try {
+				_ = Type.GetType($"SomeAntics.API.Mod, {Path.Combine(LibraryDirectory, "SomeAntics.dll")}", true);
+			}
+			catch (Exception e) {
+				if (File.Exists(Path.Combine(ExecutableDirectory, "SomeAntics.dll"))) {
+					CommandLineUtilities.WriteLineWithColor("Failed to load a modded version of SomeAntics.dll." +
+					                                        "\nMake sure you're executing Antics.SomeLoader alongside your modded copy!",
+						ConsoleColor.DarkRed);
+					CommandLineUtilities.WriteLineWithColor($"Full error: {e}", ConsoleColor.Red);
+					CommandLineUtilities.WriteLineWithColor("Press <enter> to exit!", ConsoleColor.DarkGray);
+					Console.ReadLine();
+					Environment.Exit(0);
+				}
+
+				CommandLineUtilities.WriteLineWithColor(
+					"Failed to locate a SomeAntics assembly (SomeAntics.dll file)." +
+					"\nMake sure you're executing Antics.SomeLoader alongside your modded copy!", ConsoleColor.DarkRed);
+				CommandLineUtilities.WriteLineWithColor($"Full error: {e}", ConsoleColor.Red);
+				CommandLineUtilities.WriteLineWithColor("Press <enter> to exit!", ConsoleColor.DarkGray);
+				Console.ReadLine();
+				Environment.Exit(0);
+			}
 		}
 	}
 }
